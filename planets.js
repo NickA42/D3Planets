58a0b4b7b82eee4bf10f7c320f3a06537884bc9e
Nicholas Alicea <nicholas.alicea@centre.edu>
2019-10-08 15:40:38 -0400

    work

--- planets.js
+++ planets.js
@@ -1,3 +1,40 @@
 
+var Names=[]
+Names.push("bob","jim","dave")
 
+
+
+
+var printnames = Function(Names)
+{ 
+    d3.select("#A1")
+      .selectAll("div")
+      .data(Names)
+      .enter()
+      .append("div")
+      .text(function(d){
+      return d;
+    })
+}
+
+var eachName = function(Names)
+{
+    d3.select("#A2")
+    .selectAll("p")
+    .data(Names)
+    .enter()
+    .append("p")
+    .text(function(d){
+        return Names[0];
+    })
+}
+
+var Promise = d3.json("planets.json")
+Promise.then(fcnsuccess,fcnfail)
+Promise.then(function(Data){
+    console.log("Data",Data);
+},
+funtion(err)
+{ console.log("fail",err);
+             }) 
  
